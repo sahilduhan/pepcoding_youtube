efb0be0169a3cded521ea3565c61af142c105d8f
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-06 01:19:56 +0530

    git update

--- 2d_array/wave_traversal.cpp
+++ 2d_array/wave_traversal.cpp
@@ -5,7 +5,24 @@ int main()
     int row = 3;
     int col = 3;
     int array[row][col] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    
+    for (int i = 0; i < col; i++)
+    {
+        if (i % 2 == 0)
+        {
+            for (int j = 0; j < row; j++)
+            {
+                cout << array[i][j] << " ";
+            }
+        }
+        else
+        {
+            for (int k = row - 1; k >= 0; k--)
+            {
+                cout << array[i][k] << " ";
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
