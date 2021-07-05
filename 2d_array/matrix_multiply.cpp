2fcabc66ef47635f9b072c682527769b0ba7829a
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-05 23:24:02 +0530

    git updaet

--- 2d_array/matrix_multiply.cpp
+++ 2d_array/matrix_multiply.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 int main()
 {
+    int row_1 = 3, col_1 = 3;
+    int matrix_1[row_1][col_1] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int row_2 = 3, col_2 = 3;
+    int matrix_2[row_2][col_2] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    
 
     return 0;
 }
