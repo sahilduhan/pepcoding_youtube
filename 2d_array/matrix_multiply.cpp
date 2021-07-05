01b3e40b4e76aa524e0a188b4146ce657ee3e426
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-06 00:10:19 +0530

    git udpate

--- 2d_array/matrix_multiply.cpp
+++ 2d_array/matrix_multiply.cpp
@@ -6,8 +6,26 @@ int main()
     int matrix_1[row_1][col_1] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     int row_2 = 3, col_2 = 3;
     int matrix_2[row_2][col_2] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    int result[col_1][row_2];
-    
-
+    int result[row_1][col_2];
+    if (col_1 == row_2)
+    {
+        int sum = 0;
+        for (int i = 0; i < row_1; i++)
+        {
+            for (int j = 0; j < col_2; j++)
+            {
+                for (int k = 0; k < row_2; k++)
+                {
+                    sum += matrix_1[i][k] * matrix_2[k][j];
+                }
+                cout << sum << " " ;
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "Invalid" << endl;
+    }
     return 0;
 }
