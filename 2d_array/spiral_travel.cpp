7cec136a873593f33c16717f3597d96147789c83
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-08 23:29:59 +0530

    git idpate

--- 2d_array/spiral_travel.cpp
+++ 2d_array/spiral_travel.cpp
@@ -2,19 +2,5 @@
 using namespace std;
 int main()
 {
-    int row = 4, col = 4;
-    int arr[row][col] = {{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}};
-    int row_start;
-    int row_end;
-    int col_start;
-    int col_end;
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            
-        }
-    }
-
-    return 0;
+   
 }
