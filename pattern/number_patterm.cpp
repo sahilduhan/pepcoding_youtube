d47a570511dee31b03c3d6ca4f887e5438f148be
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-13 11:15:51 +0530

    git udpaet

--- pattern/number_patterm.cpp
+++ pattern/number_patterm.cpp
@@ -3,8 +3,18 @@ using namespace std;
 int main()
 {
     int row = 5;
-    
-
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k <= i; k++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
