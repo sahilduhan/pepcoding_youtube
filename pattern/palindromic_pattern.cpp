9966a131c5efd5c7657dcd5ecf08ea3d65075b5d
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-13 12:33:47 +0530

    git update

--- pattern/palindromic_pattern.cpp
+++ pattern/palindromic_pattern.cpp
@@ -3,6 +3,18 @@ using namespace std;
 int main()
 {
     int rows = 5;
-    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int k = 0; k < rows - i; k++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i; j++)
+        {
+            cout << i << " ";
+        }
+        // for(int l=)
+    }
+
     return 0;
 }
