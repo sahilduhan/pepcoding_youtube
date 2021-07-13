cd21ca153cce1cba886f04b0c62bb80074f30899
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-13 10:45:24 +0530

    git psuh

--- pattern/rohmnus.cpp
+++ pattern/rohmnus.cpp
@@ -3,7 +3,19 @@ using namespace std;
 int main()
 {
     int rows = 5;
-    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < rows - i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 0; k < rows; k++)
+        {
+            cout << "*"
+                 << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
