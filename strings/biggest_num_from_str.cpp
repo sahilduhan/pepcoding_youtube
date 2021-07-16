f1450a414fc68e6da03518adc4f86bed99cacf03
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-17 00:41:25 +0530

    git dpaet

---
+++ strings/biggest_num_from_str.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string sahil = "21453524";
+    sort(sahil.begin(), sahil.end(), greater<int>());
+    cout << sahil << endl;
+    
+    return 0;
+}
