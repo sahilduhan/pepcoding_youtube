8cd1e1e2a1a2ef563af7509c2deaefcd7eae596c
Sahil Duhan <duhan.sahil16@gmail.com>
2021-07-17 13:05:29 +0530

    git udate

---
+++ linked_list/deletion.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int data)
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+void print_list(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
+int main()
+{
+    node *head = new node(100);
+    node *second = new node(200);
+    node *third = new node(300);
+    node *fourth = new node(400);
+    node *fifth = new node(500);
+    node *sixth = new node(600);
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    fifth->next = sixth;
+    sixth->next = NULL;
+    return 0;
+}
