34e703debb9a739da7c892e3fe36fefb8e36c792
Sahil Duhan <duhan.sahil16@gmail.com>
2021-08-03 01:12:08 +0530

    git update

---
+++ binary_search_tree/print_in_range.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+    }
+};
+int main()
+{
+    Node *root = new Node(40);
+    
+    //left side of the tree
+
+    root->left = new Node(20);
+    root->left->left = new Node(10);
+    root->left->right = new Node(30);
+    root->left->right->left = new Node(25);
+    root->left->right->right = new Node(35);
+
+    // right side of the tree
+
+    root->right = new Node(60);
+    root->right->left = new Node(50);
+    root->right->right = new Node(70);
+    root->right->right->right = new Node(80);
+
+    return 0;
+}
