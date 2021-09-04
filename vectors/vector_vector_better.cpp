0a311681ffb792a22539c5780389db6921d10fa6
Sahil Duhan <duhan.sahil16@gmail.com>
2021-09-04 17:25:14 +0530

    git update

---
+++ vectors/vector_vector_better.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    vector<vector<int>> test;
+    for (int i = 0; i < test.size(); i++)
+    {
+        int num;
+        test.push_back(vector<int>());
+        for (int j = 0; j < num; j++)
+        {
+            int val;
+            cin >> val;
+            test[i].push_back(val);
+        }
+    }
+
+    return 0;
+}
